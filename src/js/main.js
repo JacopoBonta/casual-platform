a7815c5e10b5ca151fd2d6130a4122e0399620cd
Jacopo Bontà <dev@bpsistema.it>
2017-08-29 17:53:44 +0200

    first commit - project initialization

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+/**
+ * The main script of the game.
+ * It coordinates the all the game objects thanks to the Phaser game engine.
+ * Up to date API reference: https://photonstorm.github.io/phaser-ce/
+ */
+
+const game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
+  // It is called first and used to load game assets
+  preload() {
+    
+  },
+  // Called once after preload(). Use it for various initializatoin of the current state.
+  create() {
+
+  },
+  // After create() completes this function is called once per frame. We refere to it as the game loop.
+  update() {
+
+  }
+});
