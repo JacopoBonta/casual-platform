0aad0099110cadf5b0d9a2f9227138cf529c10a6
Jacopo Bontà <bonta.jacopo@gmail.com>
2017-10-04 12:47:54 +0200

    addeed init function and level difficult; added specific postions for ground, player and level's start and level's end

--- src/js/states/game/GamePlayState.js
+++ src/js/states/game/GamePlayState.js
@@ -2,24 +2,44 @@ define(["require", "exports", "states/StateAbstract", "characters/Hero", "Platfo
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     class GamePlayState extends StateAbstract_1.default {
+        init(difficult = 2) {
+            if (difficult <= 0) {
+                this.difficult = 1;
+            }
+            else if (difficult >= 10) {
+                this.difficult = 10;
+            }
+            else {
+                this.difficult = difficult;
+            }
+        }
         preload() {
             this.game.load.image('platform', 'assets/platform_1024x1024.png');
             this.game.load.image('background', 'assets/treesbackground.png');
             this.game.load.spritesheet('hero', 'assets/hero_spritesheet.png', 21, 40);
         }
         create() {
-            this.printGameInfo();
+            this.levelStartPos = new Phaser.Point(0, this.world.height - 40);
+            this.levelEndPos = new Phaser.Point(this.world.width - 1, this.world.height - 40);
+            this.playerStartPos = new Phaser.Point(0, this.world.height - 40);
+            let groundStart = new Phaser.Point(0, this.world.height - 21);
+            let groundEnd = new Phaser.Point(this.world.width, this.world.height - 21);
             this.game.add.sprite(0, 0, 'background');
             this.ground = new Platformer_1.default(this.game, 'platform');
             this.ground
-                .generatePlatform(0, this.world.bottom - 21, this.world.width / 21, 7)
-                .generatePlatformFromArray(600, 400, [0, -1, -1, 0, 0, -1, -1])
+                .generatePlatform(groundStart, groundEnd, 10 - this.difficult)
                 .setImmovable(true);
-            this.hero = new Hero_1.default(this.game);
+            this.hero = new Hero_1.default(this.game, this.playerStartPos);
             this.cursors = this.game.input.keyboard.createCursorKeys();
             this.lifeText = this.game.add.text(32, 32, `Life: ${this.hero.getLife()}`, {
                 font: 'Indie Flower',
                 fontSize: 35,
+                fontWeight: 'bold',
+                fill: '#ff0044'
+            });
+            let levelText = this.game.add.text(this.world.centerX, 32, `The difficult for this level is ${this.difficult}`, {
+                font: 'Indie Flower',
+                fontSize: 26,
                 fontWeight: 'bold',
                 fill: '#ff0044'
             });
@@ -43,23 +63,35 @@ define(["require", "exports", "states/StateAbstract", "characters/Hero", "Platfo
             if (!hitGround) {
                 player.fall();
             }
+            if (player.getPos().x <= 0) {
+                player.goRight();
+            }
+            else if (player.getPos().x >= this.game.world.width) {
+                player.goLeft();
+            }
             if (player.getPos().y >= this.game.world.height) {
                 player.damage();
                 this.updateLifeText();
                 if (player.getLife() <= 0) {
                     this.game.state.clearCurrentState();
                     this.game.state.start("GameoverState");
+                    return;
                 }
                 else {
                     this.game.camera.flash(0x000000, 200);
-                    player.setPos();
+                    player.setPos(new Phaser.Point(0, this.world.height - 50));
                 }
+            }
+            if (player.collidePoint(this.levelEndPos)) {
+                console.log("Good job! You reach the end!");
+                player.body.velocity.x = 0;
+                this.game.state.clearCurrentState();
+                this.game.state.start("GamePlayState", true, false, this.difficult + 1);
             }
         }
         render() {
-        }
-        printGameInfo() {
-            console.log(`World height: ${this.game.world.height}\nWorld width: ${this.game.world.width}`);
+            this.game.debug.geom(this.levelStartPos, 'rgba(255, 0, 0, 1)');
+            this.game.debug.geom(this.levelEndPos, 'rgba(0, 255, 0, 1)');
         }
         updateLifeText() {
             this.lifeText.setText(`Life: ${this.hero.getLife()}`);
--- src/js/states/game/GamePlayState.ts
+++ src/js/states/game/GamePlayState.ts
@@ -6,10 +6,24 @@ import Hero from 'characters/Hero';
 import Platformer from 'Platformer';
 
 export default class GamePlayState extends State {
+    difficult :number;
     ground :Platformer;
     hero: Hero;
     cursors :Phaser.CursorKeys;
     lifeText :Phaser.Text;
+    levelStartPos :Phaser.Point;
+    levelEndPos :Phaser.Point;
+    playerStartPos :Phaser.Point;
+    
+    init(difficult :number = 2) {
+        if (difficult <= 0) {
+            this.difficult = 1;
+        } else if (difficult >= 10) {
+            this.difficult = 10;
+        } else {
+            this.difficult = difficult;
+        }
+    }
 
     preload() {
         this.game.load.image('platform', 'assets/platform_1024x1024.png');
@@ -18,17 +32,24 @@ export default class GamePlayState extends State {
     }
 
     create() {
-        this.printGameInfo();
+        this.levelStartPos = new Phaser.Point(0, this.world.height - 40);
+        this.levelEndPos = new Phaser.Point(this.world.width - 1, this.world.height - 40);
+        this.playerStartPos = new Phaser.Point(0, this.world.height - 40);
+        let groundStart = new Phaser.Point(0, this.world.height - 21);
+        let groundEnd = new Phaser.Point(this.world.width, this.world.height - 21);
+
+
+
         this.game.add.sprite(0, 0, 'background');
 
         this.ground = new Platformer(this.game, 'platform');
         this.ground
-            .generatePlatform(0, this.world.bottom - 21, this.world.width / 21, 7)
+            .generatePlatform(groundStart, groundEnd, 10 - this.difficult)
             // .generatePlatformFromArray(380, this.world.bottom - 21, [1,2,2,3,4,5,6,7,8])
-            .generatePlatformFromArray(600, 400, [0,-1,-1,0,0,-1,-1])
+            // .generatePlatformFromArray(600, 400, [0,-1,-1,0,0,-1,-1])
             .setImmovable(true);
         
-        this.hero = new Hero(this.game);
+        this.hero = new Hero(this.game, this.playerStartPos);
 
         // create the cursor key object
         this.cursors = this.game.input.keyboard.createCursorKeys();
@@ -36,6 +57,13 @@ export default class GamePlayState extends State {
         this.lifeText = this.game.add.text(32, 32, `Life: ${this.hero.getLife()}`, {
             font: 'Indie Flower',
             fontSize: 35,
+            fontWeight: 'bold',
+            fill: '#ff0044'
+        });
+
+        let levelText = this.game.add.text(this.world.centerX, 32, `The difficult for this level is ${this.difficult}`, {
+            font: 'Indie Flower',
+            fontSize: 26,
             fontWeight: 'bold',
             fill: '#ff0044'
         });
@@ -63,6 +91,12 @@ export default class GamePlayState extends State {
             player.fall();
         }
 
+        if (player.getPos().x <= 0) {
+            player.goRight();
+        } else if (player.getPos().x >= this.game.world.width) {
+            player.goLeft();
+        }
+
         // check if the player fell down 
         if(player.getPos().y >= this.game.world.height){
             player.damage();
@@ -70,19 +104,25 @@ export default class GamePlayState extends State {
             if (player.getLife() <= 0) {
                 this.game.state.clearCurrentState();
                 this.game.state.start("GameoverState");
+                return;
             } else {
                 this.game.camera.flash(0x000000, 200);
-                player.setPos();
+                player.setPos(new Phaser.Point(0, this.world.height - 50));
             }
+        }
+
+        // Check if the player have reach the end of the stage
+        if (player.collidePoint(this.levelEndPos)) {
+            console.log("Good job! You reach the end!");
+            player.body.velocity.x = 0;
+            this.game.state.clearCurrentState();
+            this.game.state.start("GamePlayState", true, false, this.difficult + 1);
         }
     }
 
     render() {
-        
-    }
-
-    printGameInfo(): void {
-        console.log(`World height: ${this.game.world.height}\nWorld width: ${this.game.world.width}`);
+        this.game.debug.geom(this.levelStartPos, 'rgba(255, 0, 0, 1)');
+        this.game.debug.geom(this.levelEndPos, 'rgba(0, 255, 0, 1)');
     }
 
     updateLifeText() {
