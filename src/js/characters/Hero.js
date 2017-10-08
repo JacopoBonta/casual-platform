3a8c966403212e6143d411e208c9da02becd1500
Jacopo Bontà <bonta.jacopo@gmail.com>
2017-10-08 15:41:32 +0200

    added getter and setter for fps property

--- src/js/characters/Hero.js
+++ src/js/characters/Hero.js
@@ -4,7 +4,7 @@ define(["require", "exports", "characters/Character"], function (require, export
     class Hero extends Character_1.default {
         constructor(game, pos, life, speed, gravity) {
             super(game);
-            this.fps = 15;
+            this._fps = 15;
             if (pos) {
                 this.spawnPos = pos;
             }
@@ -18,10 +18,17 @@ define(["require", "exports", "characters/Character"], function (require, export
                 this.gravity = gravity;
             }
             this.sprite = this.game.add.sprite(this.spawnPos.x, this.spawnPos.y, 'hero', 0);
+            this.sprite.scale.setTo(1.25, 1.25);
             this.setupSprite();
             this.game.physics.arcade.enable(this.sprite);
             this.body = this.sprite.body;
             this.setupPhysiscs();
+        }
+        get fps() {
+            return this._fps;
+        }
+        set fps(fps) {
+            this._fps = fps;
         }
         setupSprite() {
             this.sprite.anchor.setTo(.5, .5);
@@ -32,7 +39,6 @@ define(["require", "exports", "characters/Character"], function (require, export
             this.sprite.animations.add('landing', [24]);
         }
         setupPhysiscs() {
-            this.body.setSize(10 / this.sprite.scale.x, 30 / this.sprite.scale.y, 5, 5);
             this.body.gravity.y = this.gravity;
             this.body.collideWorldBounds = false;
         }
@@ -50,12 +56,12 @@ define(["require", "exports", "characters/Character"], function (require, export
             this.stop();
         }
         left(velocity) {
-            this.sprite.scale.setTo(-1, 1);
+            this.sprite.scale.setTo(-1.25, 1.25);
             this.sprite.animations.play('run');
             this.moveLeft(velocity);
         }
         right(velocity) {
-            this.sprite.scale.setTo(1, 1);
+            this.sprite.scale.setTo(1.25, 1.25);
             this.sprite.animations.play('run');
             this.moveRight(velocity);
         }
--- src/js/characters/Hero.ts
+++ src/js/characters/Hero.ts
@@ -1,6 +1,6 @@
 import Character from "characters/Character"
 export default class Hero extends Character {
-    private fps :number = 15;
+    private _fps :number = 15;
 
     constructor(game :Phaser.Game, pos ?:Phaser.Point, life ?:number, speed ?:number, gravity ?:number) {
         super(game);
@@ -17,11 +17,18 @@ export default class Hero extends Character {
             this.gravity = gravity;
         }
         this.sprite = this.game.add.sprite(this.spawnPos.x, this.spawnPos.y, 'hero', 0);
+        this.sprite.scale.setTo(1.25, 1.25);
         this.setupSprite();
         // Enable physics on the sprite
         this.game.physics.arcade.enable(this.sprite);
         this.body = this.sprite.body;
         this.setupPhysiscs();
+    }
+    get fps() :number {
+        return this._fps;
+    }
+    set fps(fps :number) {
+        this._fps = fps;
     }
     /**
      * Helper function that setup the hero's sprite
@@ -39,7 +46,7 @@ export default class Hero extends Character {
      * Helper function that setup the heros' body
      */
     private setupPhysiscs() :void {
-        this.body.setSize(10 / this.sprite.scale.x, 30 / this.sprite.scale.y, 5, 5);
+        // this.body.setSize(10 / this.sprite.scale.x, 30 / this.sprite.scale.y, 5, 5);
         this.body.gravity.y = this.gravity;
         this.body.collideWorldBounds = false;
     }
@@ -62,13 +69,13 @@ export default class Hero extends Character {
     }
 
     public left(velocity ?:number) :void {
-        this.sprite.scale.setTo(-1, 1);
+        this.sprite.scale.setTo(-1.25, 1.25);
         this.sprite.animations.play('run');
         this.moveLeft(velocity);
     }
 
     public right(velocity ?:number) :void {
-        this.sprite.scale.setTo(1, 1);
+        this.sprite.scale.setTo(1.25, 1.25);
         this.sprite.animations.play('run');
         this.moveRight(velocity);
     }
